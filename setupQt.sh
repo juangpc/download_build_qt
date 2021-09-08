42f977d88147296ed9713e22f2647275d2f4588b
juangpc <juangpc@gmail.com>
2021-09-08 17:45:46 -0400

    create directory for download code

--- setupQt.sh
+++ setupQt.sh
@@ -3,4 +3,27 @@
 
 CURRENT_DIR=`pwd`
 SCRIPT_NAME=$(basename "$0")
+SCRIPT_PATH="$(
+    cd "$(dirname "$0")" >/dev/null 2>&1
+    pwd -P
+)"
+BASE_PATH=${SCRIPT_PATH}/../..
+
+# create a new directory ./qt5 if it doesn't exist already.
+QT5DIR=qt5
+if [[ -d QT5DIR ]]; then
+echo "  "
+read -p "Directory $QT5DIR already exists. Do you want to delete it?   " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    # do dangerous stuff
+    rm -fr qt5
+else 
+    DOWNLOAD=NO
+fi 
+fi
+
+git clone ${q} -b $QTVER https://code.qt.io/qt/qt5.git
+
+
 
